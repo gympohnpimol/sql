e4509dc31680ae00f37145d0b36dd609de8446cd
gympohnpimol <gympohnpimol@gmail.com>
2020-08-04 03:34:37 +0900

    window function

---
+++ sql_WindowFunction.sql
@@ -0,0 +1,75 @@
+/*Window Function*/
+SELECT standard_amt_usd,
+       SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
+FROM orders;
+
+SELECT standard_amt_usd,
+       DATE_TRUNC('year', occurred_at) as year,
+       SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
+FROM orders;
+
+/*Ranking*/
+SELECT 
+	id,
+    account_id,
+    total,
+    RANK() OVER (PARTITION BY account_id ORDER BY total DESC) AS ranking
+FROM orders;
+
+/*Aggregates in Window Functions with and without ORDER BY*/
+SELECT id,
+       account_id,
+       standard_qty,
+       DATE_TRUNC('month', occurred_at) AS month,
+       DENSE_RANK() OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS dense_rank,
+       SUM(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS sum_std_qty,
+       COUNT(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS count_std_qty,
+       AVG(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS avg_std_qty,
+       MIN(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS min_std_qty,
+       MAX(standard_qty) OVER (PARTITION BY account_id ORDER BY DATE_TRUNC('month',occurred_at)) AS max_std_qty
+FROM orders;
+
+/*Shorten Your Window Function Queries by Aliasing*/
+SELECT id,
+       account_id,
+       DATE_TRUNC('year',occurred_at) AS year,
+       DENSE_RANK() OVER main_window AS dense_rank,
+       total_amt_usd,
+       SUM(total_amt_usd) OVER main_window AS sum_total_amt_usd,
+       COUNT(total_amt_usd) OVER main_window AS count_total_amt_usd,
+       AVG(total_amt_usd) OVER main_window AS avg_total_amt_usd,
+       MIN(total_amt_usd) OVER main_window AS min_total_amt_usd,
+       MAX(total_amt_usd) OVER main_window AS max_total_amt_usd
+FROM orders
+
+WINDOW main_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at)) 
+
+/*Comparing a Row to Previous Row*/
+SELECT account_id,
+       standard_sum,
+       LAG(standard_sum) OVER (ORDER BY standard_sum) AS lag,
+       LEAD(standard_sum) OVER (ORDER BY standard_sum) AS lead,
+       standard_sum - LAG(standard_sum) OVER (ORDER BY standard_sum) AS lag_difference,
+       LEAD(standard_sum) OVER (ORDER BY standard_sum) - standard_sum AS lead_difference
+FROM (
+SELECT account_id,
+       SUM(standard_qty) AS standard_sum
+  FROM orders 
+ GROUP BY 1
+ ) sub;
+
+ /*Percentiles with Partitions*/
+ SELECT account_id, standard_qty, occurred_at,
+NTILE(4) OVER (PARTITION BY account_id ORDER BY standard_qty) AS std_percentiles
+FROM orders
+ORDER BY account_id DESC;
+
+SELECT account_id, occurred_at, gloss_qty, 
+NTILE(2) OVER (PARTITION BY account_id ORDER BY gloss_qty) AS gls_percentiles
+FROM orders 
+ORDER BY account_id DESC;
+
+SELECT account_id, occurred_at, total_amt_usd,
+NTILE(100) OVER (PARTITION BY account_id ORDER BY total_amt_usd) AS total_percentiles
+FROM orders
+ORDER BY account_id DESC;
