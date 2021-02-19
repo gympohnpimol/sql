47dbad227031a1dbc1ada4f5a6935e0cb9fa6074
gympohnpimol <gympohnpimol@gmail.com>
2021-02-19 18:14:41 +0900

    Exercise02

--- Exercise02_sql/Exercise02.sql
+++ Exercise02_sql/Exercise02.sql
@@ -91,8 +91,39 @@ WHERE D.Budget IN (
 )
 
 -- 2.16
--- Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. 
+-- Add a new department called "Quality Assurance", 
+-- with a budget of $40,000 and departmental code 11. 
 -- Add an employee called "Mary Moore" in that department, with SSN 847-21-9811.
+
+INSERT INTO Department2(Code, Name, Budget) VALUES (11, 'Quality Assurance', 40000)
+INSERT INTO Employee2(SSN, Name, LastName, Department) VALUES (847219811, 'Mary', 'Moore', 11)
+
+-- 2.17
+-- Reduce the budget of all departments by 10%.
+UPDATE Department2
+    SET Budget = 0.9*Budget
+
+-- 2.18
+-- Reassign all employees from the Research department (code 77) 
+-- to the IT department (code 14).
+UPDATE Employee2
+    SET Department = 14
+    WHERE Department = 77
+
+-- 2.19
+-- Delete from the table all employees in the IT department (code 14).
+DELETE FROM Employee2
+    WHERE Department = 14
+
+-- 2.20
+-- Delete from the table all employees 
+-- who work in departments with a budget greater than or equal to $60,000.
+DELETE FROM Employee2
+WHERE Department IN (
+    SELECT Code
+    FROM Department2
+    WHERE Budget >= 60000
+)
 
 
 
